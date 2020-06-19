fd698113d7805f6e7dfe4bb59e6dbd628d792c9b
Lester <lester.deguzman@tup.edu.ph>
2020-06-19 15:14:24 +0800

    Finish Part 1

--- App.js
+++ App.js
@@ -1,19 +1,12 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+
+import Screen from "./app/components/Screen";
+import LoginScreen from "./app/screens/LoginScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Screen>
+      <LoginScreen />
+    </Screen>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

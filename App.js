2841db23ed15f42ca31597bf5b174486e9110b5b
Lester <lester.deguzman@tup.edu.ph>
2020-06-22 10:22:56 +0800

    FiX:

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
+import "react-native-gesture-handler";
 import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
 
 import Screen from "./app/components/Screen";
-import LoginScreen from "./app/screens/LoginScreen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import AuthNavigator from "./app/navigation/AuthNavigator";
 
 export default function App() {
   return (
     <Screen>
-      <LoginScreen />
+      <NavigationContainer>
+        <AuthNavigator />
+      </NavigationContainer>
     </Screen>
   );
 }
